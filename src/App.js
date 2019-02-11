fc956d28192a78e6546a04b85fac04745744a87e
MaxAltena <Max@MaxAltena.com>
2019-02-11 15:23:35 +0100

    Level1

--- src/App.js
+++ src/App.js
@@ -1,32 +1,36 @@
 import React, { Component } from "react";
-import "./App.css";
-import Person from "./components/Person";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: "Max"
+    };
+  }
+
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
   render() {
+    const { name } = this.state;
     return (
       <div className="App">
         <div className="container">
-          <Person
-            name="Dylano"
-            age="22"
-            study="ICT"
-            work="McDonalds"
-            place="Blerick"
-            // picture={photoDylano}
-            about="I am Dylano Hartman"
-            status="Crippling depression"
-          />
-          <Person
-            name="Max"
-            age="20"
-            study="ICT"
-            work="Geen"
-            place="Best"
-            // picture={photoMax}
-            about="I am Max Altena"
-            status="Crippling depression 2.0"
-          />
+          <h1>
+            Welcome{" "}
+            <input
+              type="text"
+              name="name"
+              value={name}
+              onChange={this.handleChange}
+            />
+          </h1>
+          This is a name: {name}
+          
         </div>
       </div>
     );
---
+++ src/components/level2/Level2.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Level2 extends Component {
+  render() {
+    return <div className="Level2">Level2</div>;
+  }
+}
+
+export default Level2;
