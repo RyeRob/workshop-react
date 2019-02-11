c1bcf8671f7436d7131b913798350bc64151d053
Dylano <dylano.hartman@gmail.com>
2019-02-11 11:32:12 +0100

    first component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import "./App.css";
+import Dylano from "./components/Dylano";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="cool">Hey</div>
+        <Dylano name="Dylano" />
       </div>
     );
   }
